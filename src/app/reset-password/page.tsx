cc8a3bd934fec183dfb936d3f7a7587eeb9ba6a2
Gabriel Cunha <75914436+GahCunha@users.noreply.github.com>
2025-03-30 12:41:25 -0300

    feat: substitui conteúdo da página de redefinição de senha por componente de carregamento correção

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -3,21 +3,8 @@ import { ResetPasswordWrapper } from "@/components/ResetPasswordWrapper";
 
 export default function ResetPasswordPage() {
   return (
-<<<<<<< HEAD
-    <div className="flex justify-center items-center min-h-screen max-w-md mx-auto p-4">
-      <div>
-        <h1 className="text-4xl font-bold mb-4">Redefinir Senha</h1>
-        {token ? (
-          <ResetPasswordForm token={token} />
-        ) : (
-          <p>Token inválido ou ausente.</p>
-        )}
-      </div>
-    </div>
-=======
     <Suspense fallback={<div>Carregando...</div>}>
       <ResetPasswordWrapper />
     </Suspense>
->>>>>>> 24cf77fd17cf02111efa0e8ccd96ef547993298e
   );
 }
